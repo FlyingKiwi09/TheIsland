2683d0c4584851f384ac11832c0c477b88e856d6
hookwaalin <hookwaalin@gs210d.AD.ECS.VUW.AC.NZ>
2022-09-15 13:45:52 +1200

    Created IslandItem Class
    Fields: xPosition, yPosition
    All things that exist in the island will extend from this class.

---
+++ TheIsland/src/IslandItem.java
@@ -0,0 +1,27 @@
+
+public class IslandItem {
+	private int xPosition, yPosition;
+
+	public IslandItem(int xPosition, int yPosition) {
+		this.xPosition = xPosition;
+		this.yPosition = yPosition;
+	}
+
+	public int getxPosition() {
+		return xPosition;
+	}
+
+	public void setxPosition(int xPosition) {
+		this.xPosition = xPosition;
+	}
+
+	public int getyPosition() {
+		return yPosition;
+	}
+
+	public void setyPosition(int yPosition) {
+		this.yPosition = yPosition;
+	}
+	
+	
+}
