949b429f2df6cd0e0ec705a47652c75c07cc09fc
hookwaalin <hookwaalin@gs210d.AD.ECS.VUW.AC.NZ>
2022-09-16 13:02:55 +1200

    Added JavaDoc to Mammal, Rabbit Class.

--- TheIsland/src/Mammal.java
+++ TheIsland/src/Mammal.java
@@ -1,4 +1,12 @@
-
+/**
+ * Mammal class is the abstract super class for all mammals on an island.
+ * <p> 
+ * Mammal class allows for subclass to do things related to being mammals including being infected with rabies and parasites. 
+ * 
+ * @author Alina Hookway
+ * @version 1.0
+ * @since 1.0
+ */
 abstract public class Mammal extends Animal {
 	
 	private boolean parasiticInfection;
--- TheIsland/src/Rabbit.java
+++ TheIsland/src/Rabbit.java
@@ -1,9 +1,15 @@
-
+/**
+ * Rabbit class represents rabbits. 
+ * <p>  
+ * 
+ * @author Alina Hookway
+ * @version 1.0
+ * @since 1.0
+ */
 public class Rabbit extends Mammal {
 
 	public Rabbit(int xPosition, int yPosition) {
 		super(xPosition, yPosition,5, 5, 2);
-		this.setSize(5);
 	}
 
 }
