0973c9e2fc10cf84b48fc2838f0a4bec2d693a47
hookwaalin <hookwaalin@gs210d.AD.ECS.VUW.AC.NZ>
2022-09-16 12:38:25 +1200

    Updated IslandSimulation class to include JavaDoc comments.
    Fixed bug in generateIsland() that was causing rabbits to be generated
    multiple times for each island when multiple islands were created. Now
    generates one set of Rabbits per Island and stores them to that island
    before storing the island to the island list.

--- TheIsland/src/IslandSimulator.java
+++ TheIsland/src/IslandSimulator.java
@@ -1,24 +1,42 @@
 import java.util.ArrayList;
 
+/**
+ * IslandSimulator class controls the creation and simulation of Islands.
+ * <p> 
+ * IslandSimulator controls the creation of Islands and other objects contained within it such as rabbits. It allows for the simulation of turns on these islands.
+ * 
+ * @author Alina Hookway
+ * @version 1.0
+ * @since 1.0
+ */
+
+
 public class IslandSimulator {
 	
 	private static ArrayList<Island> islands = new ArrayList<Island>();
 	
+	// constructor - parameter specifices the number of islands to be created in this simulation
 	public IslandSimulator(int numberOfIslands) {
 		for (int i = 0; i < numberOfIslands; i++) {
 			generateIsland();
 		}
 	}
 	
+	/**
+	 * generateIsland() creates a new Island 
+	 * <p> 
+	 * Creates a new Island including it's associated IslandItems such as rabbits and stores these to the IslandSimulator.
+	 */
+	
+	// generates an island and it's IslandItems
 	public void generateIsland() {
-		islands.add(new Island());
-		for (Island i: islands) {
-			generateRabbits(i);
-		}
+		Island newIsland = new Island();
+		generateRabbits(newIsland);
+		islands.add(newIsland);
 	}
 	
 	// generates rabbits at random positions on the island
-	public void generateRabbits(Island island) {
+	private void generateRabbits(Island island) {
 		
 		int numberOfRabbits = randomNum(5,10); // so that 5 to 10 rabbits will be generated on the island
 		
@@ -34,7 +52,7 @@ public class IslandSimulator {
 		System.out.println(islands.get(0).getLength() + " " + islands.get(0).getWidth());
 	}
 	
-	public int randomNum(int min, int max) {
+	private int randomNum(int min, int max) {
 		return (int) (Math.random() * (max - min)) + min;
 	}
 
