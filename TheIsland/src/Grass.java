c32e11fefbfef47b5cf6fdd8345cd08680be3ebf
hookwaalin <hookwaalin@gs210d.AD.ECS.VUW.AC.NZ>
2022-09-17 17:33:03 +1200

    Grass class added.
    - Grass has an initial size set randomly between 1 and 5.
    - grow() method added which increases size by 1.

---
+++ TheIsland/src/Grass.java
@@ -0,0 +1,29 @@
+/**
+ * Grass class represents grass at a particular point on the island. 
+ * <p>  
+ * Grass, like all subclasses of LivingThing, can grow, be eaten and die. Grass has an intial size between 1 and 5.
+ * 
+ * @author Alina Hookway
+ * @version 1.0
+ * @since 1.0
+ */
+public class Grass extends Foliage {
+
+	public Grass(int xPosition, int yPosition) {
+		super(xPosition, yPosition, randomNum(1, 5)); // a grass of random size between 1 and 5
+	}
+	
+	/**
+	 * grow method increases the size of the grass by 1
+	 * 
+	 * @since 1.0
+	 */
+	public void grow() {
+		this.setSize(this.getSize()+1);
+	}
+	
+	private static int randomNum(int min, int max) {
+		return (int) (Math.random() * (max - min)) + min;
+	}
+
+}
