842a24ccd71193856a6403c43d576f82c796226a
hookwaalin <hookwaalin@gs210d.AD.ECS.VUW.AC.NZ>
2022-09-17 18:41:23 +1200

    Created an abstract Bird Class that extends Animal
    variables:
    - maxHeight i.e. how high the bird can fly set in subclasses
    - nest i.e. stores a nest object, initially set to null
    
    Methods
    - fly

---
+++ TheIsland/src/Bird.java
@@ -0,0 +1,18 @@
+
+abstract public class Bird extends Animal {
+
+	private int maxHeight;
+	private Nest nest;
+	
+	public Bird(int xPosition, int yPosition, int size, int maxSpeed, int currentSpeed, int maxHeight) {
+		super(xPosition, yPosition, size, maxSpeed, currentSpeed);
+		this.maxHeight = maxHeight;
+		this.nest = null;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void fly() {
+		// this method allows the bird to go anywhere on the island, it checks that the bird can fly high enough to get over any obstacles. 
+	}
+
+}
